13364681ad5463ef32941f03efe3b489cf1a9736
Tyler Mayoff <tyler@tylermayoff.com>
2022-08-14 12:36:57 -0400

    added more code

--- Hasher.cpp
+++ Hasher.cpp
@@ -4,10 +4,36 @@
 #include <pigpio.h>
 
 void callbackFunc(int pin, int level, uint32_t tick, void *user) {
-    std::cout << "Received" << std::endl;
+
+    Hasher *self = (Hasher*) user;
+    user->_callback(pin, level, tick);
 }
 
 Hasher::Hasher(int pin, int timeout): pin(pin), timeout(timeout) {
     gpioSetMode(pin, PI_INPUT);
     gpioSetAlertFuncEx(pin, callbackFunc, (void*)this);
+}
+
+
+void Hasher::callback(int pin, int level, uint32_t tick) {
+    if (level != PI_TIMEOUT) {
+        if (!inCode) {
+            inCode = true;
+
+            gpioSetWatchdog(pin, timeout);
+
+            edges = 1;
+        } else {
+            edges++;
+        }
+    } else {
+        if (inCode) {
+            inCode = false;
+
+            gpioSetWatchdog(pin, 0);
+            if (edges > 12) {
+                std::cout << "SIGNAL?" << std::endl;
+            }
+        }
+    }
 }
--- Hasher.hpp
+++ Hasher.hpp
@@ -1,13 +1,19 @@
 #ifndef HASHER_HPP
 #define HASHER_HPP
 
+#include <cstdint>
+
 class Hasher {
   public:
     Hasher(int pin, int timeout);
 
+    void callback(int pin, int level, uint32_t tick);
+
   private:
+    bool inCode = false;
     int pin;
     int timeout;
+    int edges;
 };
 
 #endif // HASHER_HPP
