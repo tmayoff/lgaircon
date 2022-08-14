c19cc3aef78065ab659b687aeb357f92b1e711c6
Tyler Mayoff <tyler@tylermayoff.com>
2022-08-14 12:53:56 -0400

    copied directly

--- Hasher.cpp
+++ Hasher.cpp
@@ -3,35 +3,77 @@
 #include <iostream>
 #include <pigpio.h>
 
-void callbackFunc(int pin, int level, uint32_t tick, void *user) {
-    Hasher *self = (Hasher*) user;
-    self->callback(pin, level, tick);
+void Hasher::_hash(int old_val, int new_val) {
+   int val;
+
+   if      (new_val < (old_val * 0.60)) val = 13;
+   else if (old_val < (new_val * 0.60)) val = 23;
+   else                                 val = 2;
+
+   hash_val ^= val;
+   hash_val *= 16777619; /* FNV_PRIME_32 */
 }
 
-Hasher::Hasher(int pin, int timeout): pin(pin), timeout(timeout) {
-    gpioSetMode(pin, PI_INPUT);
-    gpioSetAlertFuncEx(pin, callbackFunc, (void*)this);
+void Hasher::_callback(int pin, int level, uint32_t tick) {
+   if (level != PI_TIMEOUT)
+   {
+      if (!inCode)
+      {
+         inCode = true;
+
+         gpioSetWatchdog(pin, timeout);
+
+         hash_val = 2166136261U; /* FNV_BASIS_32 */
+
+         edges = 1;
+
+         t1 = 0;
+         t2 = 0;
+         t3 = 0;
+         t4 = tick;
+      }
+      else
+      {
+         edges++;
+
+         t1 = t2;
+         t2 = t3;
+         t3 = t4;
+         t4 = tick;
+
+         if (edges > 3) _hash(t2-t1, t4-t3);
+      }
+   }
+   else
+   {
+      if (inCode)
+      {
+         inCode = false;
+
+         gpioSetWatchdog(pin, 0);
+
+         if (edges > 12) /* Anything less is probably noise. */
+         {
+            std::cout << "SIGNLA?" << std::endl;
+            // (mycallback)(hash_val);
+         }
+      }
+   }
 }
 
-void Hasher::callback(int pin, int level, uint32_t tick) {
-    if (level != PI_TIMEOUT) {
-        if (!inCode) {
-            inCode = true;
+void Hasher::_callback(int gpio, int level, uint32_t tick, void *user) {
+   /*
+      Need a static callback to link with C.
+   */
 
-            gpioSetWatchdog(pin, timeout);
+   Hasher *mySelf = (Hasher *) user;
 
-            edges = 1;
-        } else {
-            edges++;
-        }
-    } else {
-        if (inCode) {
-            inCode = false;
+   mySelf->_callback(gpio, level, tick); /* Call the instance callback. */
+}
 
-            gpioSetWatchdog(pin, 0);
-            if (edges > 12) {
-                std::cout << "SIGNAL?" << std::endl;
-            }
-        }
-    }
+Hasher::Hasher(int pin, int timeout) : pin(pin), timeout(timeout) {
+
+   gpioSetMode(pin, PI_INPUT);
+
+   gpioSetAlertFuncEx(pin, _callback, (void *)this);
 }
--- Hasher.hpp
+++ Hasher.hpp
@@ -7,13 +7,18 @@ class Hasher {
   public:
     Hasher(int pin, int timeout);
 
-    void callback(int pin, int level, uint32_t tick);
+    static void _callback(int gpio, int level, uint32_t tick, void *user);
+    void _callback(int pin, int level, uint32_t tick);
+
+    void _hash(int old_val, int new_val);
 
   private:
     bool inCode = false;
     int pin;
     int timeout;
     int edges;
+    uint32_t hash_val;
+    uint32_t t1, t2, t3, t4;
 };
 
 #endif // HASHER_HPP
