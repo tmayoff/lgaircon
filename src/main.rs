dc8ef3f5a54d4a64be680a2c820cd41e4d4f9e29
Tyler Mayoff <tyler@tylermayoff.com>
2022-08-16 23:13:26 -0400

    Kind of working IR

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{thread, time};
+
 fn main () {
     let ret = liblirc_client_sys::init("lgaircon", 1);
     if ret == -1 {
@@ -11,12 +13,34 @@ fn main () {
         return;
     }
 
+    // std::thread::spawn(|| {
+    //     let fd = liblirc_client_sys::get_local_socket(None, false);
+    //     if fd.is_err() {
+    //         println!("Failed to get socket");
+    //         return;
+    //     }
+        
+    //     loop {
+    //         println!("Sending IR...");
+    //         let r = liblirc_client_sys::send_one(fd.unwrap(), String::from("LG_AC"),  String::from("AC_LOW_23"));
+    //         if r == -1 {
+    //             println!("Failed to send");
+    //         }
+
+    //         println!("Sent IR.");
+    //         thread::sleep(time::Duration::from_secs(3)); 
+    //     }
+    // });
+    
+
     loop {
+        println!("Receiving IR....");
         let ret_c = liblirc_client_sys::nextcode();
         if ret_c.is_err() {
             println!("failed to get next code\n");
             break;
         }
+        println!("Received IR.");
 
         let raw = ret_c.expect("String Failed");
 
