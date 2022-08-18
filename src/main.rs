51d35ce48aef12f013926f42eb441c3e052e090f
Tyler Mayoff <tyler@tylermayoff.com>
2022-08-17 22:26:57 -0400

    Working tx/rx

--- src/main.rs
+++ src/main.rs
@@ -13,31 +13,30 @@ fn main () {
         return;
     }
 
-    // std::thread::spawn(|| {
-    //     let fd = liblirc_client_sys::get_local_socket(None, false);
-    //     if fd.is_err() {
-    //         println!("Failed to get socket");
-    //         return;
-    //     }
-        
-    //     loop {
-    //         println!("Sending IR...");
-    //         let r = liblirc_client_sys::send_one(fd.unwrap(), String::from("LG_AC"),  String::from("AC_LOW_23"));
-    //         if r == -1 {
-    //             println!("Failed to send");
-    //         }
+    std::thread::spawn(|| {
+        let fd = liblirc_client_sys::get_local_socket("/var/run/lirc/lircd-tx", false);
+        if fd.is_err() {
+            println!("\n");
+            return;
+        }
 
-    //         println!("Sent IR.");
-    //         thread::sleep(time::Duration::from_secs(3)); 
-    //     }
-    // });
-    
+        loop {
+            println!("Sending IR...");
+            let r = liblirc_client_sys::send_one(fd.unwrap(), "LG_AC",  "AC_ON");
+            if r == -1 {
+                println!("Failed to send");
+            }
+
+            println!("Sent IR.");
+            thread::sleep(time::Duration::from_secs(1));
+        }
+    });
 
     loop {
         println!("Receiving IR....");
         let ret_c = liblirc_client_sys::nextcode();
         if ret_c.is_err() {
-            println!("failed to get next code\n");
+            // println!("failed to get next code\n");
             break;
         }
         println!("Received IR.");
