3b29c6d09854b1a77c0ae0fa0ccff66a86957c35
Tyler Mayoff <tyler@tylermayoff.com>
2022-09-13 21:29:13 -0400

    some extra files

--- scripts/deploy_zero.sh
+++ scripts/deploy_zero.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-cross build --target arm-unknown-linux-gnueabihf --release && scp target/arm-unknown-linux-gnueabihf/release/lgaircon lgaircon@aircon.local:/home/lgaircon/lgaircon/aircon_cross
+cross build --target arm-unknown-linux-gnueabihf --release && scp target/arm-unknown-linux-gnueabihf/release/lgaircon lgaircon@aircon.local:/home/lgaircon/.local/bin/lgaircon
