6367338946dd8a96c3657d5f496e1c550a94c3d4
Tyler Mayoff <tyler@tylermayoff.com>
2022-09-30 22:26:19 -0400

    fixed DB startup

--- src/db.rs
+++ src/db.rs
@@ -1,8 +1,8 @@
 use diesel::prelude::*;
 use diesel::SqliteConnection;
+use diesel_migrations::embed_migrations;
 use diesel_migrations::EmbeddedMigrations;
 use diesel_migrations::MigrationHarness;
-use diesel_migrations::embed_migrations;
 
 use crate::db::models::NewSetting;
 use crate::db::models::NewTemperature;
@@ -18,13 +18,21 @@ pub struct DB {
 }
 
 impl DB {
-
     pub fn new() -> Self {
-        let connection = SqliteConnection::establish("app.sqlite").expect("Failed to create/connect to DB");
-
-        DB {
-            connection
+        let home_dir = std::env::var_os("HOME");
+        let dir_prefix: String;
+        match home_dir {
+            Some(dir) => dir_prefix = dir.into_string().expect("Directory failed to be converted"),
+            None => dir_prefix = String::from("/root"),
         }
+
+        let db_path = dir_prefix + "/app.sqlite";
+        println!("Using DB file {}", db_path);
+
+        let connection =
+            SqliteConnection::establish(db_path.as_str()).expect("Failed to create/connect to DB");
+
+        DB { connection }
     }
 
     pub fn run_migrations(&mut self) {
@@ -33,32 +41,30 @@ impl DB {
 
     pub fn add_setting<'a>(&mut self, name: &'a str, value: &'a str) {
         use schema::state;
-        let new_state = NewSetting {
-            name,
-            val: value,
-        };
+        let new_state = NewSetting { name, val: value };
 
         let ret = diesel::insert_into(state::table)
-        .values(&new_state)
-        .execute(&mut self.connection);
+            .values(&new_state)
+            .execute(&mut self.connection);
         ret.expect("Error adding new setting");
     }
 
     pub fn update_setting<'a>(&mut self, name: &'a str, value: &'a str) {
         use schema::state;
-        let new_state = NewSetting {
-            name,
-            val: value,
-        };
+        let new_state = NewSetting { name, val: value };
 
-        let ret = diesel::update(state::table.find(name)).set(&new_state).execute(&mut self.connection);
+        let ret = diesel::update(state::table.find(name))
+            .set(&new_state)
+            .execute(&mut self.connection);
         ret.expect("Error updating setting");
     }
 
-    pub fn get_setting (&mut self, name: &str) -> Option<String> {
+    pub fn get_setting(&mut self, name: &str) -> Option<String> {
         use schema::state;
 
-        let ret = state::table.filter(state::name.eq(name)).load::<self::models::Setting>(&mut self.connection);
+        let ret = state::table
+            .filter(state::name.eq(name))
+            .load::<self::models::Setting>(&mut self.connection);
         match ret {
             Err(_) => {
                 return None;
@@ -74,7 +80,7 @@ impl DB {
         }
     }
 
-    pub fn get_state (&mut self) -> lg_ac::State {
+    pub fn get_state(&mut self) -> lg_ac::State {
         let mut s = lg_ac::State::default();
 
         if let Some(m) = self.get_setting("mode") {
@@ -106,13 +112,11 @@ impl DB {
 
     pub fn new_temp(&mut self, temp: f64) {
         use schema::temperature;
-        let new_temp = NewTemperature {
-            value: temp
-        };
+        let new_temp = NewTemperature { value: temp };
 
         let ret = diesel::insert_into(temperature::table)
-        .values(new_temp)
-        .execute(&mut self.connection);
+            .values(new_temp)
+            .execute(&mut self.connection);
         ret.expect("Error adding temperature");
     }
 
@@ -134,7 +138,9 @@ impl DB {
 
         match self.get_setting("target_temp") {
             None => self.add_setting("target_temp", new_state.target_temp.to_string().as_str()),
-            Some(_) => self.update_setting("target_temp", new_state.target_temp.to_string().as_str()),
+            Some(_) => {
+                self.update_setting("target_temp", new_state.target_temp.to_string().as_str())
+            }
         }
 
         match self.get_setting("fan_speed") {
